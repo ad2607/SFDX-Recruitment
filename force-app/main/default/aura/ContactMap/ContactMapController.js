cd7ed2e123ef07fcf1750aa940ade32f1065a296
Adam O'Keeffe <ad2607@live.ie>
2021-01-15 13:41:36 +0000

    added map component to vacancy object

---
+++ force-app/main/default/aura/ContactMap/ContactMapController.js
@@ -0,0 +1,23 @@
+({
+    onContactsLoaded: function( component, event, helper ) {
+        var mapMarkers = [];
+        var contacts = event.getParam( 'contacts' );
+        for ( var i = 0; i < contacts.length; i++ ) {
+            var contact = contacts[i];
+            var marker = {
+                'location': {
+                    'Street': contact.MailingStreet,
+                    'City': contact.MailingCity,
+                    'PostalCode': contact.MailingPostalCode
+                },
+                'title': contact.Name,
+                'description': (
+                    'Phone: ' + contact.Phone
+                ),
+                'icon': 'standard:location'
+            };
+            mapMarkers.push( marker );
+        }
+        component.set( 'v.mapMarkers', mapMarkers );
+    }
+})
